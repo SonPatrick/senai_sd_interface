578c8a671b7028c052545cf44ed8d0513ee3b8d8
Patrick Son <pmacedo10_@hotmail.com>
2023-04-27 13:06:29 -0300

    Adicionada correção

--- lib/screens/criar_tarefa.dart
+++ lib/screens/criar_tarefa.dart
@@ -63,38 +63,183 @@ class _CriarTarefaState extends State<CriarTarefa> with AutomaticKeepAliveClient
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(25),
-        child: Column(children: [
-          TextField(
-            controller: _titleCtrl,
-            decoration: InputDecoration(
-              fillColor: Colors.white,
-              filled: true,
-              hintText: 'Título da tarefa',
-              contentPadding: const EdgeInsets.all(15),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _titleCtrl,
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Título da tarefa',
+                  contentPadding: const EdgeInsets.all(15),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onChanged: (value) {},
               ),
-            ),
-            onChanged: (value) {},
-          ),
-          SizedBox(height: 10),
-          TextField(
-            controller: _descricaoCtrl,
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            decoration: InputDecoration(
-              fillColor: Colors.white,
-              filled: true,
-              hintText: 'Descrição da tarefa',
-              contentPadding: const EdgeInsets.all(15),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
+              SizedBox(height: 10),
+              TextField(
+                controller: _descricaoCtrl,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Descrição da tarefa',
+                  contentPadding: const EdgeInsets.all(15),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onChanged: (value) {},
               ),
-            ),
-            onChanged: (value) {},
-          ),
-          SizedBox(height: 10),
-        ]),
+              SizedBox(height: 10),
+              Container(
+                width: 500,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              'Prioridade: ',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: DropdownButton(
+                                underline: SizedBox.shrink(),
+                                alignment: Alignment.topCenter,
+                                borderRadius: BorderRadius.circular(8),
+                                dropdownColor: Colors.white,
+                                value: currentPrioridade,
+                                items: _prioridade
+                                    .map<DropdownMenuItem<String>>(
+                                      (e) => DropdownMenuItem(
+                                        value: e,
+                                        child: Container(
+                                          width: 200,
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(e),
+                                        ),
+                                        alignment: Alignment.center,
+                                      ),
+                                    )
+                                    .toList(),
+                                onChanged: (String? value) => setState(
+                                  () {
+                                    if (value != null) currentPrioridade = value;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                    SizedBox(height: 10),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              'Status: ',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: DropdownButton(
+                                underline: SizedBox.shrink(),
+                                alignment: Alignment.topCenter,
+                                borderRadius: BorderRadius.circular(8),
+                                dropdownColor: Colors.white,
+                                value: currentStatus,
+                                items: _status
+                                    .map<DropdownMenuItem<String>>(
+                                      (e) => DropdownMenuItem(
+                                        value: e,
+                                        child: Container(
+                                          width: 200,
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(e),
+                                        ),
+                                        alignment: Alignment.center,
+                                      ),
+                                    )
+                                    .toList(),
+                                onChanged: (String? value) => setState(
+                                  () {
+                                    if (value != null) currentStatus = value;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                    SizedBox(height: 10),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              'Tipos: ',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: DropdownButton(
+                                underline: SizedBox.shrink(),
+                                alignment: Alignment.topCenter,
+                                borderRadius: BorderRadius.circular(8),
+                                dropdownColor: Colors.white,
+                                value: currentTipo,
+                                items: _tipos
+                                    .map<DropdownMenuItem<String>>(
+                                      (e) => DropdownMenuItem(
+                                        value: e,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Container(
+                                              width: 200,
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(e)),
+                                        ),
+                                        alignment: Alignment.center,
+                                      ),
+                                    )
+                                    .toList(),
+                                onChanged: (String? value) => setState(
+                                  () {
+                                    if (value != null) currentTipo = value;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                  ],
+                ),
+              ),
+            ]),
       ),
       bottomNavigationBar: Container(
         color: Colors.white,
@@ -104,128 +249,11 @@ class _CriarTarefaState extends State<CriarTarefa> with AutomaticKeepAliveClient
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                'Prioridade: ',
-                style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
-              ),
-              Container(
-                height: 40,
-                width: 200,
-                margin: EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: DropdownButton(
-                    underline: SizedBox.shrink(),
-                    alignment: Alignment.topCenter,
-                    borderRadius: BorderRadius.circular(8),
-                    dropdownColor: Colors.white,
-                    value: currentPrioridade,
-                    items: _prioridade
-                        .map<DropdownMenuItem<String>>(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                            alignment: Alignment.center,
-                          ),
-                        )
-                        .toList(),
-                    onChanged: (String? value) => setState(
-                      () {
-                        if (value != null) currentPrioridade = value;
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                height: 30,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              ),
-              SizedBox(width: 50),
-              Text(
-                'Tipo: ',
-                style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
-              ),
-              Container(
-                height: 40,
-                width: 200,
-                margin: EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: DropdownButton(
-                    underline: SizedBox.shrink(),
-                    alignment: Alignment.topCenter,
-                    borderRadius: BorderRadius.circular(8),
-                    dropdownColor: Colors.white,
-                    value: currentTipo,
-                    items: _tipos
-                        .map<DropdownMenuItem<String>>(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                            alignment: Alignment.center,
-                          ),
-                        )
-                        .toList(),
-                    onChanged: (String? value) => setState(
-                      () {
-                        if (value != null) currentTipo = value;
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(width: 50),
-              Text(
-                'Status: ',
-                style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
-              ),
-              Container(
-                height: 40,
-                width: 200,
-                margin: EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: DropdownButton(
-                    underline: SizedBox.shrink(),
-                    alignment: Alignment.topCenter,
-                    borderRadius: BorderRadius.circular(8),
-                    dropdownColor: Colors.white,
-                    value: currentStatus,
-                    items: _status
-                        .map<DropdownMenuItem<String>>(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                            alignment: Alignment.center,
-                          ),
-                        )
-                        .toList(),
-                    onChanged: (String? value) => setState(
-                      () {
-                        if (value != null) currentStatus = value;
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              Spacer(),
-              VerticalDivider(),
               Spacer(),
               OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                  ),
                   onPressed: () {
                     Navigator.pop(context);
                   },
